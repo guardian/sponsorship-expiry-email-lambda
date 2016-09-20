c075053189471f43617def93f2fe9aadaeae194a
Stephen Wells <stephen.wells@guardian.co.uk>
2016-09-20 13:02:49 +0100

    render found sponsorships as html body

--- functions/sendExpiryEmail/main.go
+++ functions/sendExpiryEmail/main.go
@@ -4,15 +4,83 @@ import (
 	//"github.com/apex/go-apex"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	//"encoding/json"
 	"github.com/aws/aws-sdk-go/aws"
+	"html/template"
+	"os"
+	"time"
+	"fmt"
 )
+
+const emailTemplate = `
+<!DOCTYPE html>
+<html>
+	<body>
+		<h3>Expiring sponsorships</h3>
+		{{range .Expiring}}
+			<div>
+			<span>{{with $asset := index .SponsorLogo.Assets 0}}<img src="{{$asset.ImageUrl}}"/>{{end}}</span>
+			<span>
+				{{.SponsorshipType}}<br />
+				Sponsor: <a href="https://tagmanager.gutools.co.uk/sponsorship/{{.Id}}">{{.SponsorName}}</a><br />
+				Expires: {{formatMillisDate .ValidTo}}
+			</span>
+			</div>
+		{{else}}
+			<p>no sponsorships expiring in the next 7 days</p>
+		{{end}}
+
+		<h3>Expired sponsorships</h3>
+		{{range .Expired}}
+			<div>
+			<span>{{with $asset := index .SponsorLogo.Assets 0}}<img src="{{$asset.ImageUrl}}"/>{{end}}</span>
+			<span>
+				{{.SponsorshipType}}<br />
+				Sponsor: <a href="https://tagmanager.gutools.co.uk/sponsorship/{{.Id}}">{{.SponsorName}}</a><br />
+				Expired: {{formatMillisDate .ValidTo}}
+			</span>
+			</div>
+		{{else}}
+			<p>no sponsorships expired in the last 7 days</p>
+		{{end}}
+	</body>
+</html>`
+
+func formatMillisDate(millis int64) string {
+	return time.Unix(millis / 1000, 0).Format(time.RFC1123)
+}
 
 func main() {
 	dynamo := dynamodb.New(session.New(&aws.Config{
 		Region: aws.String("eu-west-1"),
 	}))
-	LoadExpiringSoon(dynamo)
+
+	expiring, err := LoadExpiringSoon(dynamo)
+
+	if (err != nil) {
+		return
+	}
+
+	expired, err := LoadExpiredRecently(dynamo)
+
+	if (err != nil) {
+		return
+	}
+
+	t, err := template.New("webpage").Funcs(template.FuncMap{"formatMillisDate": formatMillisDate,}).Parse(emailTemplate)
+
+	templateData := struct {
+		Expiring []Sponsorship
+		Expired []Sponsorship
+	}{
+		Expiring: expiring,
+		Expired: expired,
+	}
+
+	err = t.Execute(os.Stdout, templateData)
+
+	if (err != nil) {
+		fmt.Println(err.Error())
+	}
 
 	//apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
 	//	dynamo := dynamodb.New(session.New())
